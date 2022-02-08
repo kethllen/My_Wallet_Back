d42fd58c87670ee8d7bba3e51b688552a1728923
kethllen <kety_andrey@hotmail.com>
2022-02-08 12:03:13 -0300

    ajustes heroku

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 import express, { json } from "express";
 import cors from "cors";
 import router from "./routes/index.js";
-
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 app.use(cors());
 app.use(json());

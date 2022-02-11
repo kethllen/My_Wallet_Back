0f7138c6510491513eda63c414eceb59ed555a4f
leo <celsolnv27@gmail.com>
2022-02-11 11:45:19 -0300

    Preparando revisão de código

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import authRouter from './authRouter.js';
-import dataRouter from './dataRouter.js';
-
-const router = Router();
-router.use(authRouter);
-router.use(dataRouter);
-
-export default router;
